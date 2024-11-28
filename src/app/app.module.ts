1c5ae3121b3a76b3090cec9bf657fd2b34a3220d
shravani301 <shravanikongaa@gmail.com>
2024-11-28 20:23:14 +0530

    student CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AddStudentComponent } from './add-student/add-student.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GetStudentsComponent } from './get-students/get-students.component';
+import { UpdateStudentComponent } from './update-student/update-student.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    GetStudentsComponent,
+    UpdateStudentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
